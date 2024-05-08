4331922f5900b4db60010cd52ac9caa66bc0802c
Iván Ramírez <ivaramlar@alum.us.es>
2024-05-08 09:43:39 +0200

    feat: Añadido Api de exchange para project

--- src/main/java/acme/components/MoneyService.java
+++ src/main/java/acme/components/MoneyService.java
@@ -87,4 +87,60 @@ public class MoneyService {
 		return result;
 	}
 
+	public MoneyExchange computeMoneyExchange2(final Money source, final String targetCurrency) {
+		assert source != null;
+		assert !StringHelper.isBlank(targetCurrency);
+
+		MoneyExchange result = null;
+		RestTemplate api;
+		ExchangeRate record;
+		String sourceCurrency, key;
+		Double sourceAmount, targetAmount, rate;
+		Money target;
+		Date moment;
+		if (!source.getCurrency().equals(targetCurrency))
+			try {
+				api = new RestTemplate();
+
+				sourceCurrency = source.getCurrency();
+				sourceAmount = source.getAmount();
+
+				String baseUrl = "https://v6.exchangerate-api.com/v6/";
+				String apiKey = "e96c9d778afac9897c670cc9";
+				String url = baseUrl + apiKey + "/pair/" + sourceCurrency + "/" + targetCurrency;
+
+				record = api.getForObject( //				
+					url, ExchangeRate.class);
+
+				assert record != null;
+				rate = record.getConversion_rate();
+				assert rate != null;
+				targetAmount = rate * sourceAmount;
+
+				target = new Money();
+				target.setAmount(targetAmount);
+				target.setCurrency(targetCurrency);
+
+				moment = new Date();
+
+				result = new MoneyExchange();
+				result.setSource(source);
+				result.setTargetCurrency(targetCurrency);
+				result.setDate(moment);
+				result.setTarget(target);
+
+				MomentHelper.sleep(1000); // HINT: need to pause the requests to the API a bit down to prevent DOS attacks
+			} catch (final Throwable oops) {
+				result = null;
+			}
+		else {
+			result = new MoneyExchange();
+			result.setSource(source);
+			result.setTargetCurrency(targetCurrency);
+			result.setDate(new Date());
+			result.setTarget(source);
+		}
+		return result;
+	}
+
 }
